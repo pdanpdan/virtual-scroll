f5df73005206be2a51d84964e40eb8008db91368
Dan Popescu <pdan.popescu@gmail.com>
2026-01-18 00:38:32 +0200

    feat: create d.ts files for published package
    Migrate to rolldown-vite.

--- packages/virtual-scroll/vite.config.ts
+++ packages/virtual-scroll/vite.config.ts
@@ -2,9 +2,16 @@ import { resolve } from 'node:path';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [ vue() ],
+  plugins: [
+    vue(),
+    dts({
+      tsconfigPath: resolve(__dirname, 'tsconfig.app.json'),
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
