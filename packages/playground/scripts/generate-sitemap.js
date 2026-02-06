bce69e9bb4aca383bd0fcfbb18c3a7d81ff6a27d
Dan Popescu <pdan.popescu@gmail.com>
2026-02-06 21:18:14 +0200

    chore: update sitemap

--- packages/playground/scripts/generate-sitemap.js
+++ packages/playground/scripts/generate-sitemap.js
@@ -29,30 +29,30 @@ function getPages(dir) {
 }
 
 const routes = getPages(PAGES_DIR);
-const lastmod = new Date().toISOString().split('T')[ 0 ];
+const lastmod = new Date().toISOString();
 
 const urls = routes
   .map((route) => {
-    let priority = 0.7;
+    let priority = 0.8;
     if (route === '') {
       priority = 1.0;
     } else if (route === 'docs/') {
       priority = 0.9;
     } else if (route.startsWith('vertical-') || route.startsWith('horizontal-') || route.startsWith('grid-')) {
-      priority = 0.8;
+      priority = 0.7;
     }
 
     return `  <url>
     <loc>${ BASE_URL }/${ route }</loc>
     <lastmod>${ lastmod }</lastmod>
-    <changefreq>monthly</changefreq>
+    <changefreq>weekly</changefreq>
     <priority>${ priority }</priority>
   </url>`;
   })
   .join('\n');
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 ${ urls }
 </urlset>`;
 
