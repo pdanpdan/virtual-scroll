7ec4c5704b55d25140ec48066f3c3e85f567431d
Dan Popescu <pdan.popescu@gmail.com>
2026-02-09 11:40:50 +0200

    docs: fix url normalization

--- packages/playground/lib/url.ts
+++ packages/playground/lib/url.ts
@@ -2,7 +2,8 @@ const BASE_URL = import.meta.env.BASE_URL;
 
 export function normalizeHref(href: string, addBase: boolean = true) {
   const url = addBase === true && href.startsWith(BASE_URL) === false ? `${ BASE_URL }/${ href }` : href;
-  return `/${ url.split('/').filter((t) => t.length > 0).join('/') }/`;
+  const split = url.split('/').filter((t) => t.length > 0);
+  return `/${ split.join('/') }${ split.at(-1)?.includes('.') ? '' : '/' }`;
 }
 
 export function matchHref(href: string, urlPathname: string) {
